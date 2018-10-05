d65982139ff56ca428fe96acdc881b6f702e340d
Aman Anand <aanand@ualberta.ca>
2018-10-04 19:58:53 -0600

    Functionality finished

---
+++ app/src/main/java/com/example/aman/aanand_feelsbook/EditEmotion.java
@@ -0,0 +1,149 @@
+package com.example.aman.aanand_feelsbook;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class EditEmotion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_emotion);
+    }
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+
+        // Fill the fields with the item clicked
+        FillValues();
+    }
+
+    private void FillValues(){
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        int position = Integer.parseInt(message);
+        Emotions emotion = MainActivity.emotions.get(position);
+
+        TextView comments_text = (EditText) findViewById(R.id.edit_comment);
+        comments_text.setText(" " + emotion.getComment());
+
+        TextView feeling_text = (TextView) findViewById(R.id.edit_feeling);
+        feeling_text.setText(" "+emotion.getFeeling());
+
+        String datetime = emotion.getDate();
+
+        String date = (datetime.split("-")[2]).split("T")[0]  ;
+        String mnth = datetime.split("-")[1];
+        String year = datetime.split("-")[0];
+        String hour = datetime.split(":")[0].split("T")[1];
+        String min = datetime.split(":")[1];
+        String sec = datetime.split(":")[2];
+
+        ((EditText)findViewById(R.id.editText_dd)).setText(date);
+        ((EditText)findViewById(R.id.editText_mm)).setText(mnth);
+        ((EditText)findViewById(R.id.editText_yy)).setText(year);
+        ((EditText)findViewById(R.id.editText_hh)).setText(hour);
+        ((EditText)findViewById(R.id.editText_min)).setText(min);
+        ((EditText)findViewById(R.id.editText_ss)).setText(sec);
+    }
+
+
+    public void SaveChanges(View view){
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        int position = Integer.parseInt(message);
+
+        String date = ((EditText) findViewById(R.id.editText_yy)).getText().toString() + "-" +
+                        ((EditText) findViewById(R.id.editText_mm)).getText().toString() + "-" +
+                        ((EditText) findViewById(R.id.editText_dd)).getText().toString() + "T" +
+                        ((EditText) findViewById(R.id.editText_hh)).getText().toString() + ":" +
+                        ((EditText) findViewById(R.id.editText_min)).getText().toString() + ":" +
+                        ((EditText) findViewById(R.id.editText_ss)).getText().toString() ;
+
+        // make changes to the date and comment of the emotion
+        Emotions emotion = MainActivity.emotions.get(position);
+//        emotion.setDate(date);
+
+        emotion.setComment(((EditText) findViewById(R.id.edit_comment)).getText().toString());
+        MainActivity.emotions.set(position,emotion);
+
+        if(!date.equals(MainActivity.emotions.get(position).date)) DevideAndConquer(position,date);
+
+        // save emotion in file
+        try{
+            FileOutputStream fos = openFileOutput(MainActivity.FILENAME,0);
+            MainActivity.saveInFile(fos,MainActivity.FILENAME);
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        finish();
+    }
+
+    // https://stackoverflow.com/questions/45987761/quicksort-divide-and-conquer/45987872
+    // By Josh Evans
+    private void DevideAndConquer(int position,String date){
+
+        ArrayList<Emotions> less = new ArrayList<Emotions>();
+        ArrayList<Emotions> equal = new ArrayList<Emotions>();
+        ArrayList<Emotions> more = new ArrayList<Emotions>();
+
+        Emotions editted_emotion =  MainActivity.emotions.get(position);
+        editted_emotion.setDate(date);
+
+        String editted_Date = MainActivity.emotions.get(position).date;
+        MainActivity.emotions.remove(position);
+
+        for (int i = 0; i < MainActivity.emotions.size(); i++) {
+            String elem_date = MainActivity.emotions.get(i).date;
+
+            if (compareDate(elem_date,editted_Date))
+                less.add(MainActivity.emotions.get(i));
+            else if (compareDate(elem_date,editted_Date))
+                equal.add(MainActivity.emotions.get(i));
+            else
+                more.add(MainActivity.emotions.get(i));
+        }
+
+        ArrayList<Emotions> sorted = new ArrayList<Emotions>(less);
+        sorted.addAll(equal);
+        sorted.add(editted_emotion);
+        sorted.addAll(more);
+
+        MainActivity.emotions=sorted;
+    }
+
+    private boolean compareDate(String date1, String date2) {
+        SimpleDateFormat newDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        boolean bool = false;
+        try {
+            if (newDate.parse(date1).before(newDate.parse(date2))) bool = true;
+            else if (newDate.parse(date1).equals(newDate.parse(date2))) bool = true;
+            else bool = false;
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return bool;
+    }
+}
--- app/src/main/java/com/example/aman/aanand_feelsbook/MainActivity.java
+++ app/src/main/java/com/example/aman/aanand_feelsbook/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.aman.aanand_feelsbook;
 
+import android.app.LauncherActivity;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.ArrayMap;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -23,36 +28,129 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.aman.aanand_feelsbook.MESSAGE";
-    private static final String FILENAME = "EmotionHistory.sav";
-    private ListView oldEmotions;
+    public static final String FILENAME = "EmotionHistory.sav";
+    public static final String FILENAME_1 = "EmotionCount.sav";
+    public ListView oldEmotions;
+
+
+    public static HashMap<String,Integer> feelingCount = new HashMap<String,Integer>();
 
     private ArrayAdapter<Emotions> adapter;
-    private ArrayList<Emotions> emotions = new ArrayList<Emotions>();
+    public static ArrayList<Emotions> emotions = new ArrayList<Emotions>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        oldEmotions = (ListView) findViewById(R.id.old_emotions);
+
+        oldEmotions.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+
+                // show dialog box for comment section
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setTitle("Feels Book");
+                builder.setMessage("Please select any one of the options");
+
+                builder.setPositiveButton("Edit Feeling", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(MainActivity.this, EditEmotion.class);
+                        intent.putExtra(EXTRA_MESSAGE, Integer.toString(position));
+                        startActivity(intent);
+                    }
+                });
+
+                builder.setNegativeButton("Delete Feeling", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        // decrement the feeling count
+                        String feeling = emotions.get(position).getFeeling();
+                        feelingCount.put(feeling,feelingCount.get(feeling)-1);
+
+                        // delete feeling from list
+                        emotions.remove(position);
+
+                        // save new list and count of emotions
+                        try{
+
+                            FileOutputStream fos = openFileOutput(FILENAME,0);
+                            saveInFile(fos,FILENAME);
+                            FileOutputStream fos_1 = openFileOutput(FILENAME_1,0);
+                            saveInFile(fos_1,FILENAME_1);
+
+                        }catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        adapter.notifyDataSetChanged();
+
+                        Toast.makeText(MainActivity.this,"Feeling: "+ ".." + " Deleted",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.setCancelable(true);
+                alert.show();
+
+            }
+        });
     }
 
     // dialog box code : https://www.youtube.com/watch?v=gLbH5bAHCiY
     // called when the user clicks any emotion and takes to the comments page
-    private void SaveEmotion(View view){
+    public void SaveEmotion(View view){
 
+        Button btn = (Button) findViewById(view.getId());
+        final String text = btn.getText().toString();
 
-        // save emotion
-        saveInFile();
+        SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        String date_formatted = date.format(new Date());
+
+        final Emotions currEmotion = new Emotions(date_formatted);
+
+        currEmotion.setComment("");
+        currEmotion.setFeeling(text);
+
+        // update the emotion ArrayList
+        emotions.add(currEmotion);
+
+        // update the emotion count
+        if(feelingCount.get(text)!=null){
+            feelingCount.put(text,  feelingCount.get(text)+1);
+        }else{
+            feelingCount.put(text, 1);
+        }
+
+        // update the list view
+        adapter.notifyDataSetChanged();
+
+        // save emotion & count in file
+        try{
+            FileOutputStream fos = openFileOutput(FILENAME,0);
+            saveInFile(fos,FILENAME);
+            FileOutputStream fos_1 = openFileOutput(FILENAME_1,0);
+            saveInFile(fos_1,FILENAME_1);
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
         // show dialog box for comment section
         final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        Button btn = (Button) findViewById(view.getId());
-        final String text = btn.getText().toString();
 
         builder.setTitle("Feels Book");
         builder.setMessage("Do you want to add any comments?");
@@ -89,41 +187,78 @@ public class MainActivity extends AppCompatActivity {
         oldEmotions.setAdapter(adapter);
     }
 
+    public void count_feelings(View view){
+        // show dialog box for comment section
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        builder.setTitle("Count of Emotions Recorded");
+
+        String message = "";
+
+        for (String key : feelingCount.keySet()) {
+            if(feelingCount.get(key)>0) message=message + key + " : " + feelingCount.get(key) + "\n\n";
+        }
+        if(message.equals("")) message = "No Emotion Recorded.";
+
+        builder.setMessage(message);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.setCancelable(true);
+        alert.show();
+    }
+
     private void loadFromFile() {
         try {
+
+            //load the emotions
             FileInputStream fis = openFileInput(FILENAME);
             InputStreamReader isr = new InputStreamReader(fis);
             BufferedReader reader = new BufferedReader(isr);
 
             Gson gson = new Gson();
             // creates a type
-            Type listTweetType = new TypeToken<ArrayList<Emotions>>(){}.getType();
-            emotions = gson.fromJson(reader, listTweetType);
+            Type listType = new TypeToken<ArrayList<Emotions>>(){}.getType();
+            emotions = gson.fromJson(reader, listType);
+
+            //load the emotion count
+            FileInputStream fis_1 = openFileInput(FILENAME_1);
+            InputStreamReader isr_1 = new InputStreamReader(fis_1);
+            BufferedReader reader_1 = new BufferedReader(isr_1);
+
+            Gson gson_1 = new Gson();
+            Type listType_1 = new TypeToken<HashMap<String,Integer>>(){}.getType();
+            feelingCount  = gson_1.fromJson(reader_1, listType_1);
 
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             emotions = new ArrayList<Emotions>();
             e.printStackTrace();
         }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
-    private void saveInFile(){
+    public static void saveInFile(FileOutputStream fos, String FileName){
         try {
-
-            FileOutputStream fos = openFileOutput(FILENAME,0);
             OutputStreamWriter osw = new OutputStreamWriter(fos);
             BufferedWriter writer = new BufferedWriter(osw);
 
             Gson gson =new Gson();
-            gson.toJson(emotions,writer);
+
+            if(FileName.equals(FILENAME)) gson.toJson(emotions,writer);
+            else if(FileName.equals(FILENAME_1)) gson.toJson(feelingCount ,writer);
+
             writer.flush();
-
             fos.close();
-
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
